e16969ced7611198394ba38cbe4c3013bc73255d
masterUNG <phrombutr@gmail.com>
2017-06-27 10:51:40 +0700

    Show Column OK

--- app/src/main/java/nakthon/soraya/calldriver/ReportPureJobFragment.java
+++ app/src/main/java/nakthon/soraya/calldriver/ReportPureJobFragment.java
@@ -8,12 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by masterUNG on 6/26/2017 AD.
  */
 
 public class ReportPureJobFragment extends Fragment{
+
+    //Explicit
+    private TextView[] textViews = new TextView[7];
+    private int[] ints = new int[]{R.id.txtColumn1, R.id.txtColumn2, R.id.txtColumn3,
+            R.id.txtColumn4, R.id.txtColumn5, R.id.txtColumn6, R.id.txtColumn7};
 
     @Nullable
     @Override
@@ -28,6 +34,9 @@ public class ReportPureJobFragment extends Fragment{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        //Show Column
+        showColumn();
+
         ListView listView = getView().findViewById(R.id.livPureJob);
 
         String[] strings = new String[]{"Test1", "Test2", "Test3", "dffd", "dfdf", "dfdff"};
@@ -35,5 +44,14 @@ public class ReportPureJobFragment extends Fragment{
                 android.R.layout.simple_list_item_1, strings);
         listView.setAdapter(stringArrayAdapter);
 
+    }
+
+    private void showColumn() {
+        MyConstant myConstant = new MyConstant();
+        String[] columnStrings = myConstant.getColumnReportPureJobFragmentStrings();
+        for (int i=0;i<ints.length;i+=1) {
+            textViews[i] = getView().findViewById(ints[i]);
+            textViews[i].setText(columnStrings[i]);
+        }
     }
 }   // Main Class
